8e7f3e0c7eb183b3d875c6e568a0d0b0b370593a
Mateusz Szewczyk <szewczyk.mateusz16@gmail.com>
2021-08-25 15:36:32 +0200

    fullBackupOnly and remove setTheme from mainactivity

--- app/src/main/java/com/example/chatapp/MainActivity.kt
+++ app/src/main/java/com/example/chatapp/MainActivity.kt
@@ -7,8 +7,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        setTheme(R.style.Theme_ChatApp)
         setContentView(R.layout.activity_main)
     }
 }
